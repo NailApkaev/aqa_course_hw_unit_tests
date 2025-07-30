d3e0efb5cdb0abf2ca583355e7b1b5237595165f
NailApkaev <renegad20@yandex.ru>
2025-07-30 14:19:50 +0300

    Corrected hw-3: task-1 and task-2

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -8,8 +8,8 @@ let n = 9;
 n = String(n);
 let nn = n + n;
 let nnn = n + n + n;
-n = +n;
+/*n = +n;
 nn = +nn;
-nnn = +nnn;
-let sum = n + nn + nnn;
+nnn = +nnn; */
+let sum = +n + +nn + +nnn;
 console.log(sum);
