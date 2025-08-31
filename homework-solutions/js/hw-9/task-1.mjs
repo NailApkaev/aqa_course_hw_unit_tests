ee969980c74ae04c41379cf9f328e2d648c6c331
NailApkaev <renegad20@yandex.ru>
2025-08-31 16:35:51 +0300

    Added hw-9 tasks 1-2

--- homework-solutions/js/hw-9/task-1.mjs
+++ homework-solutions/js/hw-9/task-1.mjs
@@ -11,10 +11,12 @@
 const character = { name: 'Barney', age: 36, gender: 'male', isQa: true };
 
 // 1
-let keyWithFourChars;
+let keyWithFourChars = Object.keys(character).filter((el) => el.length === 4);
+console.log(keyWithFourChars);
 
 // 2
-let stringValues;
+let stringValues = Object.values(character).filter((el) => typeof(el) === "string");
+console.log(stringValues);
 
 // 3
 /* Do not touch this part */
@@ -22,10 +24,16 @@ const logSpy = jest.spyOn(console, 'log');
 // ---
 
 // Ваш код
-let keyValuePairs;
-
+let keyValuePairs = Object.entries(character);
+// Object.entries(character).forEach((entries) => {
+//   const [key, value] = entries;
+//   console.log (`key = ${key}, value = ${value}`);
+// });
+Object.entries(character).forEach(([key, value]) =>
+  console.log (`key = ${key}, value = ${value}`)
+)
 // 4
-let hasSalaryKey1stOption;
-let hasSalaryKey2ndOption;
+let hasSalaryKey1stOption = "salary" in character;
+let hasSalaryKey2ndOption = Object.hasOwn(character, "salary");
 
 export { keyWithFourChars, stringValues, hasSalaryKey1stOption, hasSalaryKey2ndOption, keyValuePairs, logSpy };
